c7a4c631ac987fccc96a62c4b60e463e9825c880
KJenifergit <109576471+KJenifergit@users.noreply.github.com>
2022-08-22 13:31:18 +0530

    Add files via upload

---
+++ AddProduct1/src/com/ProductModel.java
@@ -0,0 +1,33 @@
+package com;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.Product;
+
+public class ProductModel {
+
+	protected SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
+
+	public boolean create(Product product) {
+		boolean result = true;
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			session.save(product);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			result = false;
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+}
